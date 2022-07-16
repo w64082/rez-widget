710412749fbae9414cd847467c47e09c5cd6a46e
Łukasz Gugała <lukaszgugala@icloud.com>
2022-07-16 19:52:56 +0200

    docker, basic schema

---
+++ app/public/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rez API</title>
+
+    <!-- Le styles -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+        body {
+            padding-top: 60px;
+            padding-bottom: 40px;
+        }
+    </style>
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+    <script src="https://cdn.jsdelivr.net/npm/html5shiv@3.7.3/dist/html5shiv.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+<div class="navbar navbar-fixed-top">
+    <div class="navbar-inner">
+        <div class="container">
+            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </a>
+            <a class="brand" href="#">Rez API</a>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+
+    <!-- Main hero unit for a primary marketing message or call to action -->
+    <div class="hero-unit">
+        <h2 style="text-transform: uppercase">Please select visit date from list</h2>
+    </div>
+
+    <!-- Example row of columns -->
+    <div class="row-fluid">
+        <table class="table table-bordered">
+            <thead style="text-transform: uppercase">
+                <th>ID</th>
+                <th>Place</th>
+                <th>Worker</th>
+                <th>Date FROM</th>
+                <th>Date TO</th>
+                <th>Make reservation</th>
+            </thead>
+        </table>
+    </div>
+
+    <hr>
+
+    <footer>
+        <p>Rez API - Rez Widget</p>
+    </footer>
+
+</div> <!-- /container -->
+
+<!--<script src="../js/jquery.js"></script>-->
+<script src="js/bootstrap.min..js"></script>
+
+</body>
+</html>
